783de330fc2bbec3677334d6792c34a1a8959795
Darkstar1756 <Darkstar1756@gmail.com>
2014-02-24 14:21:02 -0700

    Fix Youtube detection

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 var blocked = "";
 var url = window.location.href;
 var newurl = "http://web.archive.org/web/20140117015342/";
-if ( url.search( 'http://www.youtube.com' ) = 0 ) {
+if ( url.search( 'http://www.youtube.com' ) = 0 || url.search( 'www.youtube.com' ) = 0 ) {
   blocked = newurl + 'http://www.youtube.com/';
-} else if ( url.search( 'http://www.youtube.com/watch?v=' ) != -1 )
+} else if ( url.search( 'http://www.youtube.com/watch?v=' ) != -1 || url.search( 'www.youtube.com/watch?v=' != 1) )
   blocked = 'http://www.youtube.com/embed/' + url.substr( 31, 11 );
 } else {
   blocked = redir.url;
