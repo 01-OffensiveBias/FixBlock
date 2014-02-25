e31cb860844ce5fad8ee45fa2ace80bff362a7f6
unknown <noah.shuart@LENOVO-1206.mtchs.org>
2014-02-25 12:27:49 -0700

    Fixed Youtube

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 var blocked = "";
 var url = window.location.href;
 var newurl = "http://web.archive.org/web/20140117015342/";
-if ( url.search( 'http://www.youtube.com' ) = 0 || url.search( 'www.youtube.com' ) = 0 ) {
-  blocked = newurl + 'http://www.youtube.com/';
-} else if ( url.search( 'http://www.youtube.com/watch?v=' ) != -1 || url.search( 'www.youtube.com/watch?v=' != 1) )
-  blocked = 'http://www.youtube.com/embed/' + url.substr( 31, 11 );
+
+if ( url.search( 'http://www.youtube.com/channel/' ) != -1 ) {
+  newPage = newurl + 'http://www.youtube.com/';
+} else if ( url.search( 'http://www.youtube.com/watch' ) != -1 ) {
+  newPage = 'http://www.youtube.com/embed/' + url.substr( 31, 11 );
 } else {
-  blocked = redir.url;
+  newPage = newurl + redir.url;
 }
-console.log("redirecting to " + newPage + "...");
+
+console.log("Redirecting to " + newPage + "...");
 window.location.href = newPage;
