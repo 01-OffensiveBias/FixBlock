a55ad853f0db57a0f73c385127de9f078f063dae
Darkstar1756 <Darkstar1756@gmail.com>
2014-02-27 21:14:35 -0700

    Make YTO

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
-var blocked = "";
+var newPage;
 var url = window.location.href;
-var newurl = "http://web.archive.org/web/20140117015342/";
 
-if ( url.search( 'http://www.youtube.com/channel/' ) != -1 ) {
-  newPage = newurl + 'http://www.youtube.com/';
-} else if ( url.search( 'http://www.youtube.com/watch' ) != -1 ) {
+if ( url.search( 'http://www.youtube.com/watch' ) != -1 ) {
   newPage = 'http://www.youtube.com/embed/' + url.substr( 31, 11 );
-} else {
-  newPage = newurl + redir.url;
 }
 
 console.log("Redirecting to " + newPage + "...");
--- script2.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('wm-ipp').style.display='none';
-
-console.log( window.location.href + ' asdfasdfasfasdfasdfasdfasdfasdfasdfasdfa' );
-if ( window.location.href.search( 'http://www.youtube.com/watch' ) != -1 ) {
-  window.location.href = 'http://www.youtube.com/embed/' + window.location.href.substr( 73, 11 );
-}
